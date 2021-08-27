3e969e661eeb9f49681fb1228538553e4c8b812f
zaibatsu-loli <christo.achkov@gmail.com>
2021-08-28 02:47:17 +0300

    feat: added bot and readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Final-Fantasy-XIV-Character-Creation-Aviability-Bot
-Final Fantasy XIV Character Creation Aviability Bot
+# Final Fantasy XIV Character Creation Aviability Bot
+Want to make a character on certain FFXIV server but don't want check 200 times a day? This tool will help you achieve that. Setup takes less than 5 mintues.
+
+# Setup
+This bot is selfhosted, so you will need to have a few things to get it running:
+1. Install Node.js https://nodejs.org/en/
+2. Create a discord bot account https://discordpy.readthedocs.io/en/stable/discord.html and create a new token
+3. Invite the discord bot account to your server
+4. Download this repository
+5. Open a CMD in it's base directory and write a `npm install`
+6. Configure the bot
+7. After the dependencies and configuration are done installing write `npm start`
+
+# Configuring the bot
+In .env you can configure the following parameters:
+1. `DISCORDJS_BOT_TOKEN` - paste your discord bot token here
+2. `USERS_TO_NOTIFY` - paste the IDs of the users you want to notify (rich click on user and then select Copy ID) and format it in the following way `USERS_TO_NOTIFY="<@USERID1> <@USERID2> etc..."`
+3. `CHANNEL_ID` - The ID of the channel you want to use (right click and copy ID on the channel name)
+4. `SERVER_NAME` - The server name you want to make your character on (Odin for example)
---
+++ src/bot.js
@@ -0,0 +1,103 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+require('dotenv').config();
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
+
+client.on('ready', () => {
+    const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+
+    const interval = 60 * 1000;
+    setInterval(async () => {
+        var result = await getByWorldName(process.env.SERVER_NAME);
+
+        if (!result.createCharacter) return;
+        channel.send(`${process.env.USERS_TO_NOTIFY} chracter creation on Odin is available!!!!!!!!`);
+
+    }, interval)
+});
+
+const getStatus = async () => {
+
+    const data = {};
+
+    const regionNames = ['jp', 'na', 'eu'];
+
+    const { data: html } = await axios.get('https://na.finalfantasyxiv.com/lodestone/worldstatus/');
+
+    let $ = cheerio.load(html, {
+        normalizeWhitespace: true,
+        decodeEntities: false
+    });
+
+    const regions = $('div > .js--tab-content');
+
+
+    /*
+    * Hierachy: Region > Server > World
+    */
+
+    /*
+    * Regions (JP, NA, EU)
+    */
+    regions.each(async (i, e) => {
+        const servers = $('li[class="world-dcgroup__item"]', e);
+        const serversData = {};
+
+        const regionName = regionNames[i];
+
+        /*
+        * Each World Server per Data Center
+        */
+        servers.each((i, el) => {
+            const serverName = $('h2[class="world-dcgroup__header"]', el).first().text();
+
+            const worlds = $('div[class="world-list__item"]', el);
+            const worldsData = [];
+
+            /*
+            * Each Home World per World Server
+            */
+            worlds.each((i, ele) => {
+                worldsData.push({
+                    name: $('div[class="world-list__world_name"]', ele).children('p').text(),
+                    status: $('i[class="world-ic__1 js__tooltip"]', ele).attr('data-tooltip').trim(),
+                    category: $('div[class="world-list__world_category"]', ele).children('p').text(),
+                    createCharacter: $('i[class="world-ic__available js__tooltip"]', ele).attr('data-tooltip') === 'Creation of New Characters Available',
+                    server: serverName,
+                    region: regionName,
+                });
+            });
+
+            serversData[serverName] = worldsData;
+
+        });
+
+        data[regionNames[i]] = serversData;
+    });
+
+    return data;
+
+};
+
+const getAllWorlds = async () => {
+    const data = await getStatus();
+    let worlds = [];
+
+    for (const region in data) {
+        for (const server in data[region]) {
+            worlds = worlds.concat(data[region][server]);
+        }
+    }
+
+    return worlds;
+};
+
+const getByWorldName = async (name) => {
+    const worlds = await getAllWorlds();
+    return worlds.find(world => world.name.toLowerCase() === name.toLowerCase());
+};
